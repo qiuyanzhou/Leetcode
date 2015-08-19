aa57c7b80ea056f87cb8335f41510488ede5020f
Andrew Wang <weiguangwong@gmail.com>
2015-08-19 12:03:43 -0700

    Initial Commit

---
+++ laioffer1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  laioffer1
+//
+//  Created by qiuyanzhou on 8/19/15.
+//  Copyright (c) 2015 qiuyanzhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
